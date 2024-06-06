93e10ec1184727e8402225cfe2148e484f95bf61
Daniel Azil <danielazil.fse@gmail.com>
2024-06-06 21:29:10 +0000

    Adhere to pycodestyle.

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""
-	A module that utilizes type-annotated function.
-"""
+"""A module that utilizes type-annotated function."""
 
 
 def add(a: float, b: float) -> float:
     """
-    	A type-annotated function add that takes a
-     	float a and a float b as arguments and returns
-     	their sum as a float.
+    A type-annotated function add that takes afloat
+    a and a float b as arguments and returnstheir
+    sum as a float.
     """
     return a + b
