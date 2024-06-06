c990f2d9661aea3a6e5b117aa5875f6b459386ee
Daniel Azil <danielazil.fse@gmail.com>
2024-06-06 16:28:59 +0000

    A ype-annotated function floor which takes a float n as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+	A module that utilizes type-annotated function.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        A function that returns the floor value
+        of the float number.
+    """
+    return math.floor(n)
