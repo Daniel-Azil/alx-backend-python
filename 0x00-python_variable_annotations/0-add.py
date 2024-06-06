ed18b333b4d648cbf8c98f38f3731949494128f8
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2024-06-06 21:20:05 +0000

    Changed function doc string

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,5 +5,9 @@
 
 
 def add(a: float, b: float) -> float:
-    '''Returns the sum'''
+    """
+    	A type-annotated function add that takes a
+     	float a and a float b as arguments and returns
+     	their sum as a float.
+    """
     return a + b
