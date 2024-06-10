dab8ae119cf61488beb13474d64776d82e562ad3
Daniel Azil <danielazil.fse@gmail.com>
2024-06-10 22:18:12 +0000

    A module that returns a asyncio.Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    A module that returns a asyncio.Task
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+        A function that returns a asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
